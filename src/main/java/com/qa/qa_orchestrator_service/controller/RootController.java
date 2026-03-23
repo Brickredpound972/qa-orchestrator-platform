d00c4c9af526ebc4acffd9e83aa2a05ba0fb733c
Jack Yilmaz <jack.yilmaz.dev@gmail.com>
2026-03-23 13:11:11 -0400

    feat: redirect root URL to dashboard

---
+++ src/main/java/com/qa/qa_orchestrator_service/controller/RootController.java
@@ -0,0 +1,13 @@
+package com.qa.qa_orchestrator_service.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class RootController {
+
+    @GetMapping("/")
+    public String root() {
+        return "redirect:/qa/dashboard";
+    }
+}
